e181fe08e7911fa46ad2b100f5ff34a1befa3934
FravashY <fravashy@gmail.com>
2025-10-09 23:12:25 +0300

    [+] init attribute resourse

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApplicationModule } from './application/application.module';
 import { ApplicationTypeModule } from './application-type/application-type.module';
+import { AttributeModule } from './attribute/attribute.module';
 
 @Module({
-  imports: [ApplicationModule, ApplicationTypeModule],
+  imports: [ApplicationModule, ApplicationTypeModule, AttributeModule],
   controllers: [AppController],
   providers: [AppService],
 })

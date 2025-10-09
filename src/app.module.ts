d2fa28de7a37743f714c60c19617ea9e9d9a871b
FravashY <fravashy@gmail.com>
2025-10-09 23:15:03 +0300

    [+] init auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ApplicationModule } from './application/application.module';
 import { ApplicationTypeModule } from './application-type/application-type.module';
 import { AttributeModule } from './attribute/attribute.module';
 import { AttributeTypeModule } from './attribute-type/attribute-type.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ApplicationModule, ApplicationTypeModule, AttributeModule, AttributeTypeModule],
+  imports: [ApplicationModule, ApplicationTypeModule, AttributeModule, AttributeTypeModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

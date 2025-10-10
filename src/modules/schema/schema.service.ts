6021d51018013e180a5f2c19951b2686b50cea49
FravashY <fravashy@gmail.com>
2025-10-10 03:46:25 +0300

    [+] schema resource conversion to crud extends

--- src/modules/schema/schema.service.ts
+++ src/modules/schema/schema.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSchemaDto } from './dto/create-schema.dto';
-import { UpdateSchemaDto } from './dto/update-schema.dto';
+import { BaseService } from '@lib/crud';
+import { Schema } from '@entities';
+import { OffsetPaginationDto } from '@common/dtos';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { BaseRepository } from '@common/database';
 
 @Injectable()
-export class SchemaService {
-  create(createSchemaDto: CreateSchemaDto) {
-    return `This action adds a new schema with: ${JSON.stringify(createSchemaDto)}`;
-  }
+export class SchemaService extends BaseService<Schema, OffsetPaginationDto> {
+  protected readonly queryName = 's';
+  protected readonly searchField = 'name';
 
-  findAll() {
-    return `This action returns all schema`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} schema`;
-  }
-
-  update(id: number, updateSchemaDto: UpdateSchemaDto) {
-    return `This action updates a #${id} schema with: ${updateSchemaDto}`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} schema`;
+  constructor(
+    @InjectRepository(Schema)
+    private schemaRepository: BaseRepository<Schema>,
+  ) {
+    super(schemaRepository);
   }
 }
